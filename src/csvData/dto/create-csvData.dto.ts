2be07e393e6ad589b92469abe819530d07c5f314
muaazaz <muaaz.azhar@gigalabs.co>
2024-01-10 18:18:26 +0500

    Fixed issue with data not sent if tag is not entered.

--- src/csvData/dto/create-csvData.dto.ts
+++ src/csvData/dto/create-csvData.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCsvDataDto {}
+import { UploadedFile } from 'src/uploaded-file/entities/uploaded-file.entity';
+
+export class CreateCsvDataDto {
+  firstName: string;
+
+  lastName: string;
+
+  contact: string;
+
+  company: string;
+
+  uploadedFile: UploadedFile;
+}
